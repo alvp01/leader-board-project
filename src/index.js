4ef4f4d3ed4fc3a33da82716d8619cde9bb04a3b
Abel Lavieri <abel.lavieri@gmail.com>
2021-08-06 13:10:04 -0400

    Add method to dis play list in DOM

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import './style.css';
 import { getScores, addScore } from './modules/scoresHandle';
+import { displayScores } from './modules/createList';
 
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', (e)=>{
   e.preventDefault();
   addScore();
-  getScores();
+  getScores().then((response)=>{
+    console.log(response);
+    displayScores(response.result);
+  });
 });
