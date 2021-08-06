43d22e17294d56f99cfe9048562bb8a18c60238f
Abel Lavieri <abel.lavieri@gmail.com>
2021-08-06 18:13:33 -0400

    Add form refresh

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { displayScores } from './modules/createList.js';
 
 const submitBtn = document.querySelector('.btn-submit');
 const refreshBtn = document.querySelector('.btn-refresh');
+const form = document.querySelector('.score-form');
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   addScore();
   getScores().then(({ result }) => {
     displayScores(result);
+    form.reset();
   });
 });
 
@@ -20,8 +22,8 @@ refreshBtn.addEventListener('click', (e) => {
   });
 });
 
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
   getScores().then(({ result }) => {
     displayScores(result);
   });
-})
+});
